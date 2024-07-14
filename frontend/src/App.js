a29a77b5d0df5b4d2a6de20b347083cfe8247ad9
KristaSmith1 <122482815+KristaSmith1@users.noreply.github.com>
2024-07-14 11:10:41 -0600

    Added  project start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,10 @@
-// this will be the app file
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>TEST CONNECTION</div>
+  );
+};
+
+export default App;
