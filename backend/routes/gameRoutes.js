7b7c449d458b7f48b89351d0489463b6afb96ad0
KristaSmith1 <122482815+KristaSmith1@users.noreply.github.com>
2024-07-14 11:13:04 -0600

    Update gameRoutes.js

--- backend/routes/gameRoutes.js
+++ backend/routes/gameRoutes.js
@@ -6,17 +6,18 @@ const gameRoutes = express.Router();
 const dbo = require("../db/connection");
 const ObjectId = require("mongodb").ObjectId;
 
-gameRoutes.route("/").get(async (req, res) => {
-    // let player enter name
-    try {
-        let db_connect = dbo.getDb();
-        let player = {
-            name: req.body.name
-        }
-        const result = db_connect.collection("hangman").insertOne(player);
-        res.jason(result);
+// let user enter name at beginning of game
+// gameRoutes.route("/").get(async (req, res) => {
+//     // let player enter name
+//     try {
+//         let db_connect = dbo.getDb();
+//         let player = {
+//             name: req.body.name
+//         }
+//         const result = db_connect.collection("hangman").insertOne(player);
+//         res.jason(result);
 
-    } catch (err) {
-        throw err;
-    }
+//     } catch (err) {
+//         throw err;
+//     }
 });;
