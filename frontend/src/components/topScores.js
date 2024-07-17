103fc35c0377caf092b30f5e75deb2f245e151e6
KristaSmith1 <122482815+KristaSmith1@users.noreply.github.com>
2024-07-16 20:33:31 -0600

    Update topScores.js
    Added a button to navigate back to game

--- frontend/src/components/topScores.js
+++ frontend/src/components/topScores.js
@@ -8,11 +8,34 @@ import { ReactSession } from "react-client-session";
 
 export default function TopScores() {
 
-    // form to ask user for name
-    return (
-        
-        <h1>Top Scores!!</h1>
+    async function onClick(e) {
+        // when the user submits the form:
+        // store user name
 
-    );
+        e.preventDefault();
+
+
+        navigate("/hangman");
+    }
+
+
+    // form to display scores
+    return (
+        <div>
+            <h3>Top Scores</h3>
+            <table style={{ marginTop: 20 }}>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Guesses</th>
+                        <th>Word Length</th>
+                    </tr>
+                </thead>
+                <tbody>PLACEHOLDER</tbody>
+            </table>
+            <br />
+            <button onClick={onClick}>Return to Game</button>
+        </div>
+
 };
 
